2d284db5b785b9383ba5f6c49ec6c82cc1e17014
tsteindl <tobias.steindl@gmx.net>
2023-05-01 10:55:45 +0200

    added timeout if internet connection timed out

--- src/main/java/FTPCommunicator.java
+++ src/main/java/FTPCommunicator.java
@@ -1,18 +1,22 @@
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
+
 public class FTPCommunicator {
 
     public static String BASE_URL = "https://www.sec.gov/Archives/";
     private static String path = "data";
 
-    public FTPCommunicator() {}
+    public FTPCommunicator() {
+    }
 
     public static String loadNavFile(String path) throws IOException, InterruptedException {
         InputStream result = loadUrl("edgar/daily-index/" + path + "index.json");
         return (result == null) ? null : getResponseDataFromStream(result);
     }
+
     public static String loadIndexFile(String path) throws IOException, InterruptedException {
         InputStream response = loadUrl("edgar/daily-index/" + path);
         return (response == null) ? null : getResponseDataFromStream(response);
@@ -27,29 +31,32 @@ public class FTPCommunicator {
         //wait to not exceed 10 requests per second
         //TODO: change this
         Thread.sleep(100);
-
-        String url = BASE_URL + path;
-        System.out.println("load url: " + url);
-        URL u = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) u.openConnection();
-        con.setRequestMethod("GET");
+        try {
+            String url = BASE_URL + path;
+            System.out.println("load url: " + url);
+            URL u = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) u.openConnection();
+            con.setRequestMethod("GET");
 //                User Agent for Bot request headers:
 //                Sample Company Name AdminContact@<sample company domain>.com);
-        con.setRequestProperty("User-Agent", "WUTIS tobias.steindl@gmx.net");
+            con.setRequestProperty("User-Agent", "WUTIS tobias.steindl@gmx.net");
 
-        int status = con.getResponseCode();
+            int status = con.getResponseCode();
 
-        if (status == 429) {
-            System.out.println("traffic limit exceeded");
-            System.out.println("waiting for 11 minutes...");
-            TimeUnit.MINUTES.sleep(11);
-            status = con.getResponseCode();
+            if (status == 429) {
+                System.out.println("traffic limit exceeded");
+                System.out.println("waiting for 11 minutes...");
+                TimeUnit.MINUTES.sleep(11);
+                status = con.getResponseCode();
+            }
+            if (status == 200) {
+                return con.getInputStream();
+            }
+        } catch (UnknownHostException e) {
+            System.out.println("Connection timed out, waiting for 30s...");
+            Thread.sleep(30000);
         }
-        if (status == 200) {
-            return con.getInputStream();
-        }else {
-            return null;
-        }
+        return null;
     }
 
     private static String getResponseDataFromStream(InputStream stream) throws IOException {
