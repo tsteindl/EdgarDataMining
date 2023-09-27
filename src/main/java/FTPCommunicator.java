9e4f30eb1783f3eada865b84b7a22bc2cb9e6c56
tsteindl <tobias.steindl@gmx.net>
2023-09-27 12:50:49 +0200

    implement (potentially bad) version of connection pooling

--- src/main/java/FTPCommunicator.java
+++ src/main/java/FTPCommunicator.java
@@ -1,3 +1,8 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -8,6 +13,13 @@ public class FTPCommunicator {
 
     public static String BASE_URL = "https://www.sec.gov/Archives/";
     private static String path = "data";
+    private static final int MAX_CONNECTIONS = 50; // Adjust as needed
+
+    // Create an HttpClient instance with connection pooling
+    private static CloseableHttpClient httpClient = HttpClients.custom()
+            .setMaxConnTotal(MAX_CONNECTIONS)
+            .setMaxConnPerRoute(MAX_CONNECTIONS)
+            .build();
 
     public FTPCommunicator() {
     }
@@ -28,6 +40,7 @@ public class FTPCommunicator {
     }
 
     private static InputStream loadUrl(String path, int delay) throws InterruptedException, IOException {
+
         //wait to not exceed 10 requests per second
         //TODO: change this
         Thread.sleep(delay);
@@ -58,6 +71,81 @@ public class FTPCommunicator {
             System.out.println("Connection timed out, waiting for 30s...");
             Thread.sleep(30000);
         }
+        return null;
+
+
+ /*
+
+        // Wait to not exceed 10 requests per second
+        // TODO: Change this
+        Thread.sleep(delay);
+
+        String url = BASE_URL + path;
+        System.out.println("load url: " + url);
+
+        try {
+            HttpGet httpGet = new HttpGet(url);
+            httpGet.addHeader("User-Agent", "WUTIS tobias.steindl@gmx.net");
+            httpGet.addHeader("Cache-Control", "no-cache");  // Set Cache-Control header to disable caching
+            httpGet.addHeader("Pragma", "no-cache");  // Set Pragma header to disable caching
+            httpGet.addHeader("Expires", "0");  // Set Expires header to disable caching
+
+
+            HttpResponse response = httpClient.execute(httpGet);
+            int status = response.getStatusLine().getStatusCode();
+
+            if (status == 429) {
+                System.out.println("Traffic limit exceeded");
+                System.out.println("Waiting for 11 minutes...");
+                TimeUnit.MINUTES.sleep(11);
+                response = httpClient.execute(httpGet); // Retry the request
+                status = response.getStatusLine().getStatusCode();
+            }
+
+            if (status == 200) {
+                return response.getEntity().getContent();
+            }
+        } catch (UnknownHostException e) {
+            System.out.println("Connection timed out, waiting for 30s...");
+            Thread.sleep(30000);
+        }
+
+        return null;
+
+  */
+    }
+
+    private static InputStream loadUrlWithConnectionPooling(String path, int delay) throws InterruptedException, IOException {
+        // Wait to not exceed 10 requests per second
+        // TODO: Change this
+        Thread.sleep(delay);
+
+        String url = BASE_URL + path;
+        System.out.println("load url: " + url);
+
+        try {
+            HttpGet httpGet = new HttpGet(url);
+            httpGet.addHeader("User-Agent", "WUTIS tobias.steindl@gmx.net");
+
+            HttpResponse response = httpClient.execute(httpGet);
+            int status = response.getStatusLine().getStatusCode();
+
+            if (status == 429) {
+                System.out.println("Traffic limit exceeded");
+                System.out.println("Waiting for 11 minutes...");
+                TimeUnit.MINUTES.sleep(11);
+                response = httpClient.execute(httpGet); // Retry the request
+                status = response.getStatusLine().getStatusCode();
+            }
+
+            if (status == 200) {
+                return response.getEntity().getContent();
+            }
+        } catch (UnknownHostException e) {
+            System.out.println("Connection timed out, waiting for 30s...");
+            Thread.sleep(30000);
+        }
+
         return null;
     }
 
